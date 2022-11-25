4a311bb36fb804df98da934c6c7de56e17cc4081
Thimo Limpert <ThimoLuca.Limpert@qsc.de>
2022-11-25 14:30:24 +0100

    add outputs

---
+++ examples/depth10/outputs.tf
@@ -0,0 +1,3 @@
+output "folders" {
+  value = module.this.folders
+}
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,14 @@
-
+output "folders" {
+  value = merge(
+    google_folder.level_0,
+    google_folder.level_1,
+    google_folder.level_2,
+    google_folder.level_3,
+    google_folder.level_4,
+    google_folder.level_5,
+    google_folder.level_6,
+    google_folder.level_7,
+    google_folder.level_8,
+    google_folder.level_9
+  )
+}
